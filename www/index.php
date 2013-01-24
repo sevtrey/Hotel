53e26f9a03032e60570112536528eb2e25e294dc
Sasha <sasha.torbek@yandex.ru>
2013-01-25 00:56:14 +0200

    Папка www.еще не доделано

---
+++ www/index.php
@@ -0,0 +1,122 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ru-RU">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Верстка сайта</title>
+	<link rel="stylesheet" href="style.css" type="text/css"  />
+	<link href="css/lightbox.css" rel="stylesheet" />  
+	
+</head>
+
+<script src="js/jquery-1.7.2.min.js"></script>
+<script src="js/lightbox.js"></script>
+
+
+<body>
+<div class="site">
+	<div class="sitetop2text">
+		<div class="topmenu">
+			<div class="navigat">
+				<div class=mitem ><li><a href=''>главная</a></li></div> 
+				<div class=mitem><li><a href=''>номера</a></li></div>
+				<div class=mitem><li><a href=''>цены</a></li></div>
+				<div class=mitem><li><a href=''>фотоальбом</a></li></div>
+				<div class=mitem><li><a href=''>контакты</a></li></div>
+			</div>	
+		</div>
+		
+		<div class="toptext">
+			
+		</div>
+	</div>
+	<div class="socbook">
+		<div class="social">
+			<div class="socialicon">
+				<div class="sb">
+					<a href='' target=_blank><img border=0 src='images/fb.png'></a>
+					<a href='' target=_blank><img border=0 src='images/vk.png'></a>
+					<a href='' target=_blank><img border=0 src='images/nz.png'></a>
+					<a href='' target=_blank><img border=0 src='images/mail.png'></a>
+				</div>
+			</div>
+		</div>
+		<div class="booking">
+			<div class="textform">
+				<form name="formochka" action="" method="post">        
+					<div class="login">Ваше имя:</br><input name="login" /></div> 
+					<div class="login">Страна и город:</br><input name="login" /></div> 	
+					<div class="login">Телефон:</br><input name="login" /></div> 
+					<div class="login">E-mail:</br><input name="login" /></div> 
+					<div class="login">Дата заезда:</br><input name="login" /></div> 
+					<div class="login">Дата выезда:</br><input name="login" /></div>
+					<div class="login">Количество человек:</br><input name="login" /></div>
+				</form> 
+			</div>
+			<div class="textformrad">
+				Питание:<br><input type="radio" name="sex" value="man" checked>в отеле
+							<input type="radio" name="sex" value="woman">самостоятельно
+			</div>
+			<div class="batton">
+				<div class="bat">
+					<a href=''><img border=0 src='images/bron.png'></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="middlephoto">
+		<div class="photogallery">
+			<div class="pageinfo">
+				<h1>ФОТОАЛЬБОМ</h1>
+			</div>
+			<div class="prewgallery">
+				<div class="prewgalleryarea">
+					<div class="prewgalleryareaitem">
+						<center>
+						<a href='' rel='' target=_blank title='Перейти в фотогалерею'><img src='images/big15.jpg' border=0 alt='фотография номера'></a><p>фотография номера</p>
+						<a href='images/big14.jpg' rel='lightbox[news]' target=_blank title='увеличить'><p>увеличить</p></a>
+						</center>
+					</div>
+					<div class="prewgalleryareaitem">
+						<center>
+						<a href='' rel='' target=_blank title='Перейти в фотогалерею'><img src='images/big15.jpg' border=0 alt='фотография номера'></a><p>фотография номера</p>
+						<a href='images/big14.jpg' rel='lightbox[news]' target=_blank title='увеличить'><p>увеличить</p></a>
+						</center>
+					</div>
+					<div class="prewgalleryareaitem">
+						<center>
+						<a href='' rel='' target=_blank title='Перейти в фотогалерею'><img src='images/big15.jpg' border=0 alt='фотография номера'></a><p>фотография номера</p>
+						<a href='images/big14.jpg' rel='lightbox[news]' target=_blank title='увеличить'><p>увеличить</p></a>
+						</center>
+					</div>
+					<div class="prewgalleryareaitem">
+						<center>
+						<a href='' rel='' target=_blank title='Перейти в фотогалерею'><img src='images/big15.jpg' border=0 alt='фотография номера'></a><p>фотография номера</p>
+						<a href='images/big14.jpg' rel='lightbox[news]' target=_blank title='увеличить'><p>увеличить</p></a>
+						</center>
+					</div>
+					<div class="prewgalleryareaitem">
+						<center>
+						<a href='' rel='' target=_blank title='Перейти в фотогалерею'><img src='images/big15.jpg' border=0 alt='фотография номера'></a><p>фотография номера</p>
+						<a href='images/big14.jpg' rel='lightbox[news]' target=_blank title='увеличить'><p>увеличить</p></a>
+						</center>
+					</div>
+					<div class="prewgalleryareaitem">
+						<center>
+						<a href='' rel='' target=_blank title='Перейти в фотогалерею'><img src='images/big15.jpg' border=0 alt='фотография номера'></a><p>фотография номера</p>
+						<a href='images/big14.jpg' rel='lightbox[news]' target=_blank title='увеличить'><p>увеличить</p></a>
+						</center>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="middletextpage">
+		<div class="textpage">
+			
+		</div>
+	</div>	
+	<div class="bottom">
+	</div>
+</div>
+</body>
+</html>
