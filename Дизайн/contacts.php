072bbf897a2f8519a025f5b1da852531a60bc199
Sasha <sasha.torbek@yandex.ru>
2013-01-27 00:46:11 +0200

    доделал.Доделай фронт end
    остальные страницы без понятия как именно надо сделать.доделай, если что
    я подредактирую остальные страницы

--- Дизайн/contacts.php
+++ Дизайн/contacts.php
@@ -15,18 +15,34 @@
 <body>
 <div class="site">
 	<div class="sitetop2text">
+		<div class="logotype"></div>
 		<div class="topmenu">
 			<div class="navigat">
-				<div class=mitem ><li><a href=''>главная</a></li></div> 
-				<div class=mitem><li><a href=''>номера</a></li></div>
-				<div class=mitem><li><a href=''>цены</a></li></div>
-				<div class=mitem><li><a href=''>фотоальбом</a></li></div>
-				<div class=mitem><li><a href=''>контакты</a></li></div>
+				<div class=mitem ><li><a href='index.php'>главная</a></li></div>
+				<div class=mitem ><img border=0 src='images/slesh.png'></div>
+				<div class=mitem><li><a href='rooms.php'>номера</a></li></div>
+				<div class=mitem ><img border=0 src='images/slesh.png'></div>
+				<div class=mitem><li><a href='prices.php'>цены</a></li></div>
+				<div class=mitem ><img border=0 src='images/slesh.png'></div>
+				<div class=mitem><li><a href='photos.php'>фотоальбом</a></li></div>
+				<div class=mitem ><img border=0 src='images/slesh.png'></div>
+				<div class=mitem><li><a href='contacts.php'>контакты</a></li></div>
 			</div>	
 		</div>
 		
-		<div class="toptext">
-			
+		<div class="toptextblock">
+			<div class="toptext" align="justify">
+				<p>Вас приветствует частный гостевой дом "Бухта Стрелецкая"!</p>
+				<p>Крымское солнце и ласковое Черное море подарят Вам
+				   незабываемые дни, а наш пансионат всегда готов предложить
+				   Вам приятный отдых по доступной цене! Частный гостевой дом
+				   &#171;Бухта Стрелецкая&#187; находится в укромном, далеком от суеты
+				   машин и толп людей в частном секторе, и в то же время 10-15
+				   мин. езды отделяют его от центра города. Гостевой дом
+				   находится в непосредственной близости от заповедника
+				   &#171;Херсонес Таврический&#187; и двух пляжей</p> 
+				<p>Желаем приятного отдыха в Крыму!</p>    
+			</div>
 		</div>
 	</div>
 	<div class="socbook">
@@ -111,11 +127,33 @@
 		</div>
 	</div>
 	<div class="middletextpage">
-		<div class="textpage">
-			
+		<div class="textpageblock">
+			<div class="textpage" align="justify">
+				<p>Новое двухэтажное здание гостиницы включает 16 номеров - 8 трехместных,
+				   8 четырехместных. Общее количество мест в гостинице -56.</p>
+				<p>Также отдыхающим предлогается утопающий в зелени каменный 2х комнатный
+				   домик со всеми удобствами (бойлер,TV,холодильник,туалет)</p>
+				<p>Условия проживания: душевые, туалеты, умывальники находятся в отдельном
+					санитарном блоке.Горячая и холодная вода постоянно.Питаются отдыхающие в 
+					собственной столовой.Есть возможность для приготовления пищи самостоятельно.</p>   
+				<p>На территории хостела: мангал, газон,беседка,клумбы,TV,много фруктовых
+					деревьев.</p>
+				<p>Городской пляжЖ Мелкая галька,песок.На пляже-прокат гидроциклов,шезлонгов,бананов.</p>
+			</div>	
 		</div>
 	</div>	
 	<div class="bottom">
+		<div class=topmenuniz>
+			<div class=mitemniz ><li><a href=''>ГЛАВНАЯ</a></li></div>
+			<div class=mitemniz ><li>/</li></div>
+		    <div class=mitemniz><li><a href=''>НОМЕРА</a></li></div>
+			<div class=mitemniz ><li>/</li></div>
+			<div class=mitemniz><li><a href=''>ЦЕНЫ</a></li></div>
+			<div class=mitemniz ><li>/</li></div>
+			<div class=mitemniz><li><a href=''>ФОТОАЛЬБОМ</a></li></div>
+			<div class=mitemniz ><li>/</li></div>
+			<div class=mitemniz><li><a href=''>КОНТАКТЫ</a></li></div>
+		</div>
 	</div>
 </div>
 </body>
---
+++ Дизайн/rooms.php
@@ -0,0 +1,160 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ru-RU">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Верстка сайта</title>
+	<link rel="stylesheet" href="style.css" type="text/css"  />
+	<link href="css/lightbox.css" rel="stylesheet" />  
+	
+</head>
+
+<script src="js/jquery-1.7.2.min.js"></script>
+<script src="js/lightbox.js"></script>
+
+
+<body>
+<div class="site">
+	<div class="sitetop2text">
+		<div class="logotype"></div>
+		<div class="topmenu">
+			<div class="navigat">
+				<div class=mitem ><li><a href='index.php'>главная</a></li></div>
+				<div class=mitem ><img border=0 src='images/slesh.png'></div>
+				<div class=mitem><li><a href='rooms.php'>номера</a></li></div>
+				<div class=mitem ><img border=0 src='images/slesh.png'></div>
+				<div class=mitem><li><a href='prices.php'>цены</a></li></div>
+				<div class=mitem ><img border=0 src='images/slesh.png'></div>
+				<div class=mitem><li><a href='photos.php'>фотоальбом</a></li></div>
+				<div class=mitem ><img border=0 src='images/slesh.png'></div>
+				<div class=mitem><li><a href='contacts.php'>контакты</a></li></div>
+			</div>	
+		</div>
+		
+		<div class="toptextblock">
+			<div class="toptext" align="justify">
+				<p>Вас приветствует частный гостевой дом "Бухта Стрелецкая"!</p>
+				<p>Крымское солнце и ласковое Черное море подарят Вам
+				   незабываемые дни, а наш пансионат всегда готов предложить
+				   Вам приятный отдых по доступной цене! Частный гостевой дом
+				   &#171;Бухта Стрелецкая&#187; находится в укромном, далеком от суеты
+				   машин и толп людей в частном секторе, и в то же время 10-15
+				   мин. езды отделяют его от центра города. Гостевой дом
+				   находится в непосредственной близости от заповедника
+				   &#171;Херсонес Таврический&#187; и двух пляжей</p> 
+				<p>Желаем приятного отдыха в Крыму!</p>    
+			</div>
+		</div>
+	</div>
+	<div class="socbook">
+		<div class="social">
+			<div class="socialicon">
+				<div class="sb">
+					<a href='' target=_blank><img border=0 src='images/fb.png'></a>
+					<a href='' target=_blank><img border=0 src='images/vk.png'></a>
+					<a href='' target=_blank><img border=0 src='images/nz.png'></a>
+					<a href='' target=_blank><img border=0 src='images/mail.png'></a>
+				</div>
+			</div>
+		</div>
+		<div class="booking">
+			<div class="textform">
+				<form name="formochka" action="" method="post">        
+					<div class="login">Ваше имя:</br><input name="login" /></div> 
+					<div class="login">Страна и город:</br><input name="login" /></div> 	
+					<div class="login">Телефон:</br><input name="login" /></div> 
+					<div class="login">E-mail:</br><input name="login" /></div> 
+					<div class="login">Дата заезда:</br><input name="login" /></div> 
+					<div class="login">Дата выезда:</br><input name="login" /></div>
+					<div class="login">Количество человек:</br><input name="login" /></div>
+				</form> 
+			</div>
+			<div class="textformrad">
+				Питание:<br><input type="radio" name="sex" value="man" checked>в отеле
+							<input type="radio" name="sex" value="woman">самостоятельно
+			</div>
+			<div class="batton">
+				<div class="bat">
+					<a href=''><img border=0 src='images/bron.png'></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="middlephoto">
+		<div class="photogallery">
+			<div class="pageinfo">
+				<h1>ФОТОАЛЬБОМ</h1>
+			</div>
+			<div class="prewgallery">
+				<div class="prewgalleryarea">
+					<div class="prewgalleryareaitem">
+						<center>
+						<a href='' rel='' target=_blank title='Перейти в фотогалерею'><img src='images/big15.jpg' border=0 alt='фотография номера'></a><p>фотография номера</p>
+						<a href='images/big14.jpg' rel='lightbox[news]' target=_blank title='увеличить'><p>увеличить</p></a>
+						</center>
+					</div>
+					<div class="prewgalleryareaitem">
+						<center>
+						<a href='' rel='' target=_blank title='Перейти в фотогалерею'><img src='images/big15.jpg' border=0 alt='фотография номера'></a><p>фотография номера</p>
+						<a href='images/big14.jpg' rel='lightbox[news]' target=_blank title='увеличить'><p>увеличить</p></a>
+						</center>
+					</div>
+					<div class="prewgalleryareaitem">
+						<center>
+						<a href='' rel='' target=_blank title='Перейти в фотогалерею'><img src='images/big15.jpg' border=0 alt='фотография номера'></a><p>фотография номера</p>
+						<a href='images/big14.jpg' rel='lightbox[news]' target=_blank title='увеличить'><p>увеличить</p></a>
+						</center>
+					</div>
+					<div class="prewgalleryareaitem">
+						<center>
+						<a href='' rel='' target=_blank title='Перейти в фотогалерею'><img src='images/big15.jpg' border=0 alt='фотография номера'></a><p>фотография номера</p>
+						<a href='images/big14.jpg' rel='lightbox[news]' target=_blank title='увеличить'><p>увеличить</p></a>
+						</center>
+					</div>
+					<div class="prewgalleryareaitem">
+						<center>
+						<a href='' rel='' target=_blank title='Перейти в фотогалерею'><img src='images/big15.jpg' border=0 alt='фотография номера'></a><p>фотография номера</p>
+						<a href='images/big14.jpg' rel='lightbox[news]' target=_blank title='увеличить'><p>увеличить</p></a>
+						</center>
+					</div>
+					<div class="prewgalleryareaitem">
+						<center>
+						<a href='' rel='' target=_blank title='Перейти в фотогалерею'><img src='images/big15.jpg' border=0 alt='фотография номера'></a><p>фотография номера</p>
+						<a href='images/big14.jpg' rel='lightbox[news]' target=_blank title='увеличить'><p>увеличить</p></a>
+						</center>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="middletextpage">
+		<div class="textpageblock">
+			<div class="textpage" align="justify">
+				<p>Новое двухэтажное здание гостиницы включает 16 номеров - 8 трехместных,
+				   8 четырехместных. Общее количество мест в гостинице -56.</p>
+				<p>Также отдыхающим предлогается утопающий в зелени каменный 2х комнатный
+				   домик со всеми удобствами (бойлер,TV,холодильник,туалет)</p>
+				<p>Условия проживания: душевые, туалеты, умывальники находятся в отдельном
+					санитарном блоке.Горячая и холодная вода постоянно.Питаются отдыхающие в 
+					собственной столовой.Есть возможность для приготовления пищи самостоятельно.</p>   
+				<p>На территории хостела: мангал, газон,беседка,клумбы,TV,много фруктовых
+					деревьев.</p>
+				<p>Городской пляжЖ Мелкая галька,песок.На пляже-прокат гидроциклов,шезлонгов,бананов.</p>
+			</div>	
+		</div>
+	</div>	
+	<div class="bottom">
+		<div class=topmenuniz>
+			<div class=mitemniz ><li><a href=''>ГЛАВНАЯ</a></li></div>
+			<div class=mitemniz ><li>/</li></div>
+		    <div class=mitemniz><li><a href=''>НОМЕРА</a></li></div>
+			<div class=mitemniz ><li>/</li></div>
+			<div class=mitemniz><li><a href=''>ЦЕНЫ</a></li></div>
+			<div class=mitemniz ><li>/</li></div>
+			<div class=mitemniz><li><a href=''>ФОТОАЛЬБОМ</a></li></div>
+			<div class=mitemniz ><li>/</li></div>
+			<div class=mitemniz><li><a href=''>КОНТАКТЫ</a></li></div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
